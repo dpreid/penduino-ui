b7626b79ef892b1c4b06a10b8e84bfbbf76b10c5
dpreid <dprydereid@gmail.com>
2020-09-04 13:15:29 +0100

    deploy

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import "bootstrap/dist/css/bootstrap.min.css";
+
 //import ReconnectingWebSocket from '../public/js/reconnecting-websocket.min.js';
 
 export const eventBus = new Vue();
