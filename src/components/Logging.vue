20594ceca6cf152d078ce678aed9389691a8527a
dpreid <dprydereid@gmail.com>
2023-01-09 15:19:03 +0000

    added in logging stream components for connecting to log websocket

---
+++ src/components/Logging.vue
@@ -0,0 +1,64 @@
+<template>
+
+<div id='logging'>
+
+</div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+    name: 'Logging',
+    props: {
+        url: String,   
+    },
+    data () {
+        return {
+            logSocket: null,
+        }
+    },
+    mounted(){
+        
+        
+    },
+    watch:{
+        url(){
+			try{
+				if(this.url != '' && this.getLogURLObtained){
+					this.connect();								
+				} else{
+					console.log('disconnecting: ' + this.url);
+				}
+			} catch(e){
+				console.log(e);
+			}
+		},
+    },
+    computed:{
+        ...mapGetters([
+            'getLogURLObtained',
+            
+        ]),
+
+    },
+    methods:{
+        connect(){
+            let _store = this.$store;
+
+			//this.logSocket = new WebSocket(this.url);
+            this.logSocket = new WebSocket('ws://127.0.0.1:8000');  //TESTING
+			_store.dispatch('setLogSocket', this.logSocket);
+            
+            this.logSocket.onopen = () => {
+				console.log('log connection opened at ', this.url);
+                
+			};
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
